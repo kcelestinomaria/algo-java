75741b9528f0fe5d235f5c8df9d99ad350d8ffb4
Celestine Kariuki <celestino@safariblocks.com>
2024-07-08 14:28:01 +0000

    sorting & searching

---
+++ BubbleSortAlgo.java
@@ -0,0 +1,28 @@
+public class BubbleSortAlgo {
+    
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            // Last i elements are already in place, so no need to check them again
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
---
+++ MergeSortAlgo.java
@@ -0,0 +1,18 @@
+public class MergeSortAlgo {
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            // Sort first and second halves
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            // Merge the sorted halves
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        // Sizes of 
+    }
+}
