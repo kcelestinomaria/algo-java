7395ea0914a4a8f382e36fd8c081c8cb19c52685
Celestine Kariuki <celestino@safariblocks.com>
2024-07-08 13:29:57 +0000

    Java Code

---
+++ BalancedParentheses.java
@@ -0,0 +1,29 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class BalancedParentheses {
+    public static boolean isBalanced(String expression){
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : expression.toCharArray()) {
+            if (ch == '(') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String expression1 = "(())";
+        String expression2 = "(()";
+
+        System.out.println("Is balanced: " + isBalanced(expression1));
+        System.out.println("Is balanced: " + isBalanced(expression2));
+    }
+}
